270d86721815300e96eb63834aa61b9ac41598e0
hmbashar <hmbashar@gmail.com>
2022-04-27 23:35:24 +0600

    custom font, color, responsive,button

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     "./public/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+         'twclass2': '#930096',
+         'twclass3' : {
+           400:'#36ff2e',
+           700: '#156f15',
+         }
+      },
+      fontFamily: {
+        'squarePeg': ['Square Peg'],
+      }
+
+    },
   },
   plugins: [],
 }
